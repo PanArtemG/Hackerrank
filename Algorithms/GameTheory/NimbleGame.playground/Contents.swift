a269318dea699c2768ed7fc94e5cd9d3d08ffe78
Artem Panasenko <52529151+PanArtemG@users.noreply.github.com>
2025-01-06 14:41:39 +0200

    add temporary file for task (#8)

---
+++ Algorithms/GameTheory/NimbleGame.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+// MARK: - Nimble Game
+
+/*
+ * Complete the 'nimbleGame' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER_ARRAY s as parameter.
+ */
+
+func nimbleGame(s: [Int]) -> String {
+    // Write your code here
+
+    return ""
+}
+
